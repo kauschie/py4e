456c946bbffa62440e7f4e5415da2e8ef0797f53
Michael Kausch <kauschie223@yahoo.com>
2020-11-23 18:56:25 -0800

    Added encapsulated with computescore() and renamed file

--- ch4_score.py
+++ ch4_score.py
@@ -1,16 +1,22 @@
 try:
     score = float(input("Enter score: "))
-    if score >= 0.9 and score <= 1.0:
-        print('A')
-    elif score >= 0.8 and score < 0.9:
-        print('B')
-    elif score >= 0.7 and score < 0.8:
-        print('C')
-    elif score >= 0.6 and score < 0.7:
-        print('D')
-    elif score >= 0 and score < 0.6:
-        print('F')
-    else:
-        print("Bad score")
 except:
     print('Bad score')
+    quit()
+
+def computescore(score):
+    if score >= 0.9 and score <= 1.0:
+        return('A')
+    elif score >= 0.8 and score < 0.9:
+        return('B')
+    elif score >= 0.7 and score < 0.8:
+        return('C')
+    elif score >= 0.6 and score < 0.7:
+        return('D')
+    elif score >= 0 and score < 0.6:
+        return('F')
+    else:
+        return("Bad score")
+
+
+print(computescore(score))
