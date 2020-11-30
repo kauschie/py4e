fe5ca5964f5bf89a7ef79357d45f7646cba91b02
Michael Kausch <kauschie223@yahoo.com>
2020-11-30 13:00:26 -0800

    added ex1

---
+++ ch6_ex1_indexing.py
@@ -0,0 +1,19 @@
+print("Method 1")
+index = 1
+fruit = 'banana'
+while index <= len(fruit):
+    letter = fruit[-index]
+    print(letter)
+    index = index + 1
+
+# OR
+
+# print("Method 2")
+
+
+# fruit = 'banana'
+# index = len(fruit)-1
+# while index >= 0:
+#     letter = fruit[index]
+#     print(letter)
+#     index -= 1
